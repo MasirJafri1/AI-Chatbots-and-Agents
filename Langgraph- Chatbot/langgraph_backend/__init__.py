5b3cc76d0f753399cf65d721cd99a36909bc5020
MasirJafri1 <masirjafri1@gmail.com>
2025-10-10 20:01:37 +0530

    revised the project structure

---
+++ Langgraph- Chatbot/langgraph_backend/__init__.py
@@ -0,0 +1,1 @@
+"""Initialize the langgraph_backend package."""
--- Langgraph- Chatbot/streamlit_frontend.py
+++ Langgraph- Chatbot/streamlit_frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langgraph_backend import chatbot
+from langgraph_backend.chatbot import chatbot
 from langchain_core.messages import HumanMessage
 
 CONFIG = {'configurable': {'thread_id':'thread-1'}}
